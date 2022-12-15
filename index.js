d73d0da13218e2c450444f1eb7c06c0e941a15d5
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-12-15 13:00:41 +0530

    Added sequelize to node project and created dev database

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (request, response) {
-  response.send('Hello World')
-})
+app.get("/", function (request, response) {
+  response.send("Hello World");
+});
 
-app.listen(3000)
+app.listen(3000);
