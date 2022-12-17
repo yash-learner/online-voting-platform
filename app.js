21428f54f55a4c5d387edbc32976834ebd6a7ae9
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-12-17 16:39:28 +0530

    Added templates for signin and signup

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+
+app.get("/signup", (request, response) => {
+    response.render("signup");
+})
+
+app.get("/login", (request, response) => {
+    response.render("login");
+})
+
 app.get("/", function (request, response) {
-  response.send("Hello World");
+  response.render("index");
 });
 
 module.exports = app;
