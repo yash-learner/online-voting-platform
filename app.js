c8abc8a697b4277aca7f83f51d670bea082f47be
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-12-17 17:24:38 +0530

    Added signup logic

--- app.js
+++ app.js
@@ -1,15 +1,103 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+const { User } = require("./models");
+const bcrypt = require('bcrypt');
+const passport = require("passport");
+// eslint-disable-next-line no-unused-vars
+const connectEnsureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser("shh! some secret string"));
 app.set("view engine", "ejs");
 
+app.use(
+  session({
+    secret: "my-super-secret-key-7218728182782818218782718hsjahsu8as8a8su88",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 24 hour
+  })
+);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    function (username, password, done) {
+      User.findOne({ where: { email: username, password: password } })
+        .then(function (user) {
+          return done(null, user);
+        })
+        .catch((error) => {
+          return done(error);
+        });
+    }
+  )
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+  console.log("Serializing user in session: ", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
 
 app.get("/signup", (request, response) => {
-    response.render("signup");
-})
+  response.render("signup");
+});
 
 app.get("/login", (request, response) => {
-    response.render("login");
+  response.render("login");
+});
+
+const saltRounds = 10;
+app.post("/users", async function (request, response) {
+  // eslint-disable-next-line no-unused-vars
+  const hashedPwd = await bcrypt.hash(request.body.password, saltRounds);
+  console.log(hashedPwd);
+  try {
+    const user = await User.create({
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
+      email: request.body.email,
+      password: hashedPwd,
+    });
+    request.login(user, function (err) {
+      if (err) {
+        console.log(err);
+      }
+      return response.redirect("/elections");
+    });
+  } catch (error) {
+    console.log(error);
+    request.flash(
+      "error",
+      error.errors.map((error) => error.message)
+    );
+    return response.redirect("/signup");
+  }
+});
+
+app.get("/elections", (request, response) => {
+    response.render("elections");
 })
 
 app.get("/", function (request, response) {
