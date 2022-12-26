c7f5aefb9558e94582e2cc00d814182264e5e6c1
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-12-26 12:40:43 +0530

    Created options table

---
+++ migrations/20221225135058-create-option.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Options', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      questionId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+    await queryInterface.addConstraint("Options", {
+      fields: ["questionId"],
+      type: "foreign key",
+      references: {
+        table: "Questions",
+        field: "id",
+      },
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Options');
+  }
+};
---
+++ models/option.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Option extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Option.init({
+    title: DataTypes.STRING,
+    questionId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Option',
+  });
+  return Option;
+};
